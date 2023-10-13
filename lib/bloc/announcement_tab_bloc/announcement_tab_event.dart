9522d3bc505fcf143611ee8cdb86d208ab1feb33
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-10-13 15:07:56 +0530

    comment added in event section

--- lib/bloc/announcement_tab_bloc/announcement_tab_event.dart
+++ lib/bloc/announcement_tab_bloc/announcement_tab_event.dart
@@ -2,16 +2,32 @@ part of 'announcement_tab_bloc.dart';
 
 @freezed
 class AnnouncementTabEvent with _$AnnouncementTabEvent {
+  //Event displaying input section
   const factory AnnouncementTabEvent.showInputSection() = ShowInputSection;
+
+  //Event ot get all annoucements
   const factory AnnouncementTabEvent.getAllAnnouncements() =
       GetAllAnnouncements;
+
+  //Event to add annoumcement
   const factory AnnouncementTabEvent.addAnnouncement() = AddAnnouncement;
+
+  //Event to pick a pdf file from storage
   const factory AnnouncementTabEvent.pickFile() = PickFile;
+
+  //Event to delete an annoumcement while cliking T icon
   factory AnnouncementTabEvent.deleteAnnouncement({required int id}) =
       DeleteAnnouncement;
+
+  //Event to show replay input
   factory AnnouncementTabEvent.showReplyInput(
       {required AnnouncementModel? announcementModel}) = ShowReplyInput;
-  const factory AnnouncementTabEvent.sendReply({required AnnouncementModel model}) = SendReply;
-  const factory AnnouncementTabEvent.like({required AnnouncementModel model}) = Like;
-}
 
+  //Event to send replay
+  const factory AnnouncementTabEvent.sendReply(
+      {required AnnouncementModel model}) = SendReply;
+
+  //Event to like
+  const factory AnnouncementTabEvent.like({required AnnouncementModel model}) =
+      Like;
+}
